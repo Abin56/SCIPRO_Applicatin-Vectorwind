3d03090449ffa2bd149423b5dcb7cc8065cd986d
LeptonProjects <leptonofficialprojects@gmail.com>
2023-11-30 19:04:36 +0530

    Home screen and video player completed -- @ edwin

--- lib/view/pages/homepage/homepage.dart
+++ lib/view/pages/homepage/homepage.dart
@@ -9,58 +9,66 @@ import 'carosel slider widget/carosel_slider.dart';
 import 'popular courses/popular_courses.dart';
 import 'recorded courses/recorded_courses.dart';
 
-
-
-
-
-
 class SciproHomePage extends StatelessWidget {
   const SciproHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(appBar: AppBar(),
+    return Scaffold(
+      appBar: AppBar(),
       body: SafeArea(
         child: SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: Container(width: double.infinity,
-          color:cWhite,  child: Padding(
-              padding:  EdgeInsets.only(left: 20.w,top: 10.h,right: 20.h),
-              child: Column(mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+          child: Container(
+            width: double.infinity,
+            color: cWhite,
+            child: Padding(
+              padding: EdgeInsets.only(left: 20.w, top: 10.h, right: 20.h),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-             
-                    Padding(
-                      padding:  EdgeInsets.only(top: 10.h),
-                      child: GooglePoppinsWidgets(text: 'Hello...', fontsize: 19.sp,fontWeight: FontWeight.w300,color: Colors.black,),
-                    ),
-                 Padding(
-                   padding:  EdgeInsets.only(top: 10.h),
-                   child: GooglePoppinsWidgets(text: 'Nainika v', fontsize: 20.sp,color: cBlack,fontWeight: FontWeight.bold,),
-                 ),
-                 Padding(
-                   padding:  EdgeInsets.only(top: 30.h,),
-                   child:  SubscribedButton(text: 'SUBSCRIBED COURSES')
-                 ),
-                   Padding(
-                     padding:  EdgeInsets.only(top: 20.h,bottom: 20),
-                     child: const CarouselSliderWidget(),
-                     
-                   ),
-                  
-                 const Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: PopularCourses(),
-          
-                ),
-                 
-                   const RecordedCourses(),
                   Padding(
-                    padding:  EdgeInsets.only(top: 10.h,bottom: 10.h),
+                    padding: EdgeInsets.only(top: 10.h),
+                    child: GooglePoppinsWidgets(
+                      text: 'Hello...',
+                      fontsize: 19.sp,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.h),
+                    child: GooglePoppinsWidgets(
+                      text: 'Nainika v',
+                      fontsize: 20.sp,
+                      color: cBlack,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(
+                        top: 30.h,
+                      ),
+                      child: GestureDetector(
+                          // onTap:()=>Get.to(()=>RecordedCourseCater) ,
+                          child: SubscribedButton(text: 'SUBSCRIBED COURSES'))),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.h, bottom: 20),
+                    child: const CarouselSliderWidget(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: PopularCourses(),
+                  ),
+                  const RecordedCourses(),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.h, bottom: 10.h),
                     child: SubscribedButton(text: "Explore Courses"),
                   )
-              ],),
-            ) ,
+                ],
+              ),
+            ),
           ),
         ),
       ),
@@ -75,6 +83,6 @@ class SciproHomePage extends StatelessWidget {
           ),
         ),
       ),
-    ) ;
+    );
   }
 }
