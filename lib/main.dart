005ac2328dbf44f8c4ef01cc7bdb7bb02cc343ba
ajay <ajayp005@gmail.com>
2023-11-30 19:31:17 +0530

    3commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:scipro_application/firebase_options.dart';
-import 'package:scipro_application/view/colors/colors.dart';
-import 'package:scipro_application/view/pages/google_signing/google_signing.dart';
+
+import 'view/pages/on_boardingScreen/introduction_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,12 +25,11 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, child) {
-        return  MaterialApp(
-          theme: ThemeData(
-          ),
+        return MaterialApp(
+          theme: ThemeData(),
           debugShowCheckedModeBanner: false,
           title: 'First Method',
-          home: GoogleSignindScreen(),
+          home: const OnBoardingPage(),
         );
       },
     );
