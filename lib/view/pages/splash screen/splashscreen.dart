30506c250ad65d743cdf5658adf07246a667e3c8
LeptonProjects <leptonofficialprojects@gmail.com>
2023-12-13 14:11:23 +0530

    13 dec testing praveen record management

--- lib/view/pages/splash screen/splashscreen.dart
+++ lib/view/pages/splash screen/splashscreen.dart
@@ -1,75 +1,17 @@
-import 'dart:async';
-
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:scipro_application/controller/auth_controller/auth_controller.dart';
-import 'package:scipro_application/view/colors/colors.dart';
 import 'package:scipro_application/view/pages/google_signing/google_signing.dart';
 import 'package:scipro_application/view/pages/home/homepage.dart';
 import 'package:scipro_application/view/pages/on_boardingScreen/introduction_screen.dart';
 
 import '../../../utils/shared_preference_class.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen>
-    with TickerProviderStateMixin {
-  double _fontSize = 2;
-  double _containerSize = 1.5;
-  double _textOpacity = 0.0;
-  double _containerOpacity = 0.0;
-
-  late AnimationController _controller;
-  late Animation<double> animation1;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 3));
-
-    animation1 = Tween<double>(begin: 40, end: 20).animate(CurvedAnimation(
-        parent: _controller, curve: Curves.fastLinearToSlowEaseIn))
-      ..addListener(() {
-        setState(() {
-          _textOpacity = 1.0;
-        });
-      });
-
-    _controller.forward();
-
-    Timer(const Duration(seconds: 2), () {
-      setState(() {
-        _fontSize = 1.06;
-      });
-    });
-
-    Timer(const Duration(seconds: 2), () {
-      setState(() {
-        _containerSize = 2;
-        _containerOpacity = 1;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
       final authController = Get.put(AuthController());
       await SharedPreferenceClass.initSp();
@@ -86,105 +28,10 @@ class _SplashScreenState extends State<SplashScreen>
     });
 
     return Scaffold(
-      backgroundColor: cWhite,
-      body: Stack(
-        children: [
-          ListView(
-            children: [
-              Center(
-                child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 2000),
-                    curve: Curves.bounceOut,
-                    height: height / _fontSize),
-              ),
-              Center(
-                child: AnimatedOpacity(
-                  duration: const Duration(milliseconds: 2000),
-                  opacity: _textOpacity,
-                  child: AnimatedTextKit(
-                    animatedTexts: [
-                      ColorizeAnimatedText('SCI PRO',
-                          textStyle: colorizeTextStyle,
-                          colors: colorizeColors,
-                          speed: const Duration(milliseconds: 100)),
-                    ],
-                    isRepeatingAnimation: true,
-                    onTap: () {
-                      print("Tap Event");
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Center(
-            child: AnimatedOpacity(
-              duration: const Duration(milliseconds: 2000),
-              curve: Curves.fastLinearToSlowEaseIn,
-              opacity: _containerOpacity,
-              child: AnimatedContainer(
-                  duration: const Duration(milliseconds: 2000),
-                  curve: Curves.fastLinearToSlowEaseIn,
-                  height: width / _containerSize,
-                  width: width / _containerSize,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: SizedBox(
-                    height: 150,
-                    width: double.infinity,
-                    child: Image.asset(
-                      'assets/images/SCIPRO.png',
-                      fit: BoxFit.fill,
-                    ),
-                  )
-                  // child: const Text(
-                  //   'YOUR APP\'S LOGO',
-                  // ),
-                  ),
-            ),
-          ),
-        ],
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.asset("assets/images/SCIPRO.png"),
       ),
     );
   }
 }
-
-class PageTransition extends PageRouteBuilder {
-  final Widget page;
-
-  PageTransition(this.page)
-      : super(
-          pageBuilder: (context, animation, anotherAnimation) => page,
-          transitionDuration: const Duration(milliseconds: 2000),
-          transitionsBuilder: (context, animation, anotherAnimation, child) {
-            animation = CurvedAnimation(
-              curve: Curves.fastLinearToSlowEaseIn,
-              parent: animation,
-            );
-            return Align(
-              alignment: Alignment.bottomCenter,
-              child: SizeTransition(
-                sizeFactor: animation,
-                axisAlignment: 0,
-                child: page,
-              ),
-            );
-          },
-        );
-}
-
-const colorizeColors = [
-  Colors.red,
-  Colors.blue,
-  Colors.yellow,
-  Colors.purple,
-];
-
-const colorizeTextStyle = TextStyle(
-  fontSize: 60.0,
-  fontWeight: FontWeight.bold,
-  fontFamily: 'Horizon',
-);
